15fadb42e6c3455257ad5447b3532b7420060ddd
Paulo Henrique Da Silva Neto <paulo.silva1989@ibm.com>
2022-07-12 08:15:07 -0300

    Initial commit

---
+++ src/main/java/udemy/nelio/cursojavaangular/CursoJavaAngularApplication.java
@@ -0,0 +1,13 @@
+package udemy.nelio.cursojavaangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoJavaAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoJavaAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/udemy/nelio/cursojavaangular/CursoJavaAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package udemy.nelio.cursojavaangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoJavaAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
