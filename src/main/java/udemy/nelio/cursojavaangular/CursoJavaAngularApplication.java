87f0f80feac6d07fb7083f43449a41235d0b9131
Paulo Henrique Da Silva Neto <paulo.silva1989@ibm.com>
2022-07-12 08:22:04 -0300

    port 8080

--- src/main/java/udemy/nelio/cursojavaangular/CursoJavaAngularApplication.java
+++ src/main/java/udemy/nelio/cursojavaangular/CursoJavaAngularApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CursoJavaAngularApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CursoJavaAngularApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CursoJavaAngularApplication.class, args);
+    }
 
 }
--- src/test/java/udemy/nelio/cursojavaangular/CursoJavaAngularApplicationTests.java
+++ src/test/java/udemy/nelio/cursojavaangular/CursoJavaAngularApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CursoJavaAngularApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
